fe70058ecdd351960b2252fc93806d35f870cfcc
david-fiaty-cko <david.fiaty@checkout.com>
2020-06-05 18:11:21 +0100

    Updated the resource models and services classes file headers

--- Model/LogReader/ResourceModel/LogEntity.php
+++ Model/LogReader/ResourceModel/LogEntity.php
@@ -1,19 +1,20 @@
 <?php
 /**
- * Naxero.com
- * Professional ecommerce integrations for Magento
+ * Checkout.com
+ * Authorized and regulated as an electronic money institution
+ * by the UK Financial Conduct Authority (FCA) under number 900816.
  *
  * PHP version 7
  *
  * @category  Magento2
- * @package   Naxero
- * @author    Platforms Development Team <contact@naxero.com>
- * @copyright Naxero.com
+ * @package   Checkout.com
+ * @author    Platforms Development Team <platforms@checkout.com>
+ * @copyright 2010-2019 Checkout.com
  * @license   https://opensource.org/licenses/mit-license.html MIT License
- * @link      https://www.naxero.com
+ * @link      https://docs.checkout.com/
  */
 
-namespace Naxero\Translation\Model\ResourceModel;
+namespace CheckoutCom\Magento2\Model\LogReader\ResourceModel;
 
 /**
  * Log entity mysql resource
